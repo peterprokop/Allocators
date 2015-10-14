0d6085a6b0db4274a9878573482135e78cfa072c
Peter Prokop <prokop.petr@gmail.com>
2015-10-14 22:56:22 +0300

    Initial Commit

---
+++ Allocators/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Allocators
+//
+//  Created by Peter Prokop on 14/10/15.
+//  Copyright © 2015 Peter Prokop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
